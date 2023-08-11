4602b18a2a45e3a75e90049ccc896dbdb9632805
Herman <hermanvasilkouskiy@gmail.com>
2023-08-11 16:15:15 +0300

    added tabs, items + script files

--- js/script.js
+++ js/script.js
@@ -1,2 +1,35 @@
-let number = 7;
-const pi = "3.14";
+$(document).ready(function(){
+    $('.carousel__slider').slick({
+        speed: 1200,
+        prevArrow: '<button type="button" class="slick-prev"><img src="icons/left.png"></button>',
+        nextArrow: '<button type="button" class="slick-next"><img src="icons/right.png"></button>',
+        responsive: [
+            {
+                breakpoint: 768,
+                settings: {
+                    dots: true,
+                    arrows: false
+                }
+            }
+        ]
+});
+    $('ul.catalog__tabs').on('click', 'li:not(.catalog__tab_active)', function() {
+        $(this)
+        .addClass('catalog__tab_active').siblings().removeClass('catalog__tab_active')
+        .closest('div.container').find('div.catalog__content').removeClass('catalog__content_active').eq($(this).index()).addClass('catalog__content_active');
+    });
+
+    function toggleSlide(item) {
+        $(item).each(function(i) {
+            $(this).on('click',function(e) {
+                e.preventDefault();
+                $('.catalog-item__content').eq(i).toggleClass('catalog-item__content_active');
+                $('.catalog-item__list').eq(i).toggleClass('catalog-item__list_active');
+            })
+        })
+    };
+
+    toggleSlide('a.catalog-item__link');
+    toggleSlide('a.catalog-item__back');
+  });
+  
