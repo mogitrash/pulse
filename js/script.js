760f00431e0637b12a0fd73cf509e8eee7e703a0
Herman <hermanvasilkouskiy@gmail.com>
2023-08-17 13:58:37 +0300

    modal windows added

--- js/script.js
+++ js/script.js
@@ -30,5 +30,29 @@ $(document).ready(function(){
 
     toggleSlide('a.catalog-item__link');
     toggleSlide('a.catalog-item__back');
+
+    $('[data-modal = consultation]').on('click',function(){
+        $('.overlay, #consultation').fadeIn();
+    });
+
+    $('.button_item').on('click',function(){
+        $('.overlay, #order').fadeIn();
+    });
+
+    // $('[data-modal = buy]').on('click',function(){
+    //     $('#order').fadeOut();
+    //     $('#thanks').fadeIn();
+    // });
+
+    $('.modal__close').on('click',function(){
+        $('.overlay, #consultation, #order, #thanks').fadeOut();
+    });
+
+    $('.button_item').each(function(i) {
+        $(this).on('click', function(){
+            $('#order .modal__descr').text($('.catalog-item__subtitle').eq(i).text())
+        });
+    }); 
+
   });
   
